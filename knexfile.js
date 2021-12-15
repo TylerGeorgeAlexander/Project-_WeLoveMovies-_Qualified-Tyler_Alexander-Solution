eb1e6daa7e7ece6db4b28aac454a5358d4045894
Tyler G Alexander <tylergeorgealexander@gmail.com>
2021-12-15 03:53:45 -0800

    working yet?

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@ const path = require("path");
 require("dotenv").config();
 
 const {
-  DATABASE_URL = "postgresql://postgres@localhost/postgres",
+  DATABASE_URL = "postgres://hzxjnjkr:iUS0aKUK1Fa1TPz1tYVLJ3axh0ClJKEy@kashin.db.elephantsql.com/hzxjnjkr",
 } = process.env;
 
 module.exports = {
